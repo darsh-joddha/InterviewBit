beadae172cd8eff68225a1e1efb026e3e3ec941e
darsh-joddha <darsh.joddha.0533@gmail.com>
2018-06-17 11:04:24 +0530

    Facebook, Amazon, Microsoft

--- Two-Pointers/3Sum.cpp
+++ Two-Pointers/3Sum.cpp
@@ -1,5 +1,17 @@
-#include<bits/stdc++.h>
 
+/*3-Sum
+
+Given an array S of n integers, find three integers in S such that the sum is closest to a given number, target.
+Return the sum of the three integers.
+
+Assume that there will only be one solution
+
+Example:
+given array S = {-1 2 1 -4},
+and target = 1.
+
+The sum that is closest to the target is 2. (-1 + 2 + 1 = 2)#include<bits/stdc++.h>
+*/
 using namespace std;
 
 int threeSumClosest(vector<int> &A, int B) {
