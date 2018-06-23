1ad42630c37d4dd7fbe2f9e84379eae0431cf0ed
darsh-joddha <darsh.joddha.0533@gmail.com>
2018-06-23 11:21:49 +0530

    Practice problem

---
+++ Stack And Queues/ReverseString.cpp
@@ -0,0 +1,39 @@
+/*
+Given a string S, reverse the string using stack.
+
+Example :
+
+Input : "abc"
+Return "cba"
+*/
+
+#include <bits/stcd++.h>
+
+using namespace std;
+
+string reverseString(string A) {
+    stack<char> a;
+    
+    for(int i = 0; i < A.size(); i++)
+    {
+        a.push(A[i]);
+    }
+    
+    string s = "";
+    
+    while(!a.empty())
+    {
+        s += a.top();
+        a.pop();
+    }
+    
+    return s;
+}
+
+int main(int argc, char const *argv[])
+{
+	string A = "abcd";
+
+	cout << reverseString(A) << endl;
+	return 0;
+}
